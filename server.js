741aac2f0a82c63b59f0cbdd376f05ca17dc0bde
Haykelnres <Igor02mir@yandex.ru>
2023-05-31 14:36:18 +0300

    Лаба 8

--- server.js
+++ server.js
@@ -1,43 +1,56 @@
-var express = require("express"),
-    http = require("http"),
-    app = express(),
-    klients = [
-    {
-    "description" : "Вавилов С.И. 21.08.2023",
-    "tags" : [ "билет", "клиент" ]
-    },
-    {
-    "description" : "Сарманов С.И. 21.09.2023",
-    "tags" : [ "билет", "клиент" ]
-    },
-    {
-    "description" : "Головенко Э.Э. 13.11.2023",
-    "tags" : [ "билет", "клиент" ]
-    },
-    {
-    "description" : "s7 airlines",
-    "tags" : [ "партнеры", "авиакомпании" ]
-    },
-    {
-    "description" : "Ural airines",
-    "tags" : [ "партнеры", "авиакомпании" ]
-    },
-    {
-    "description" : "Turkish airlines",
-    "tags" : [ "партнеры", "авиакомпании" ]
-    }
-    ];
-    
 
-app.use(express.static(__dirname + "/lr7"));
+const express = require("express");
+const http = require("http"); 
+const mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var app = express()
 http.createServer(app).listen(3000);
-app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/lr7"));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/klients", function(req, res) {
-    res.json(klients);
-})
+mongoose.connect("mongodb://0.0.0.0:27017/Flights", { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log('db connected...');
+        })
+        .catch(() => {
+            console.log('bad connection...');
+        });
 
-app.post("/klients", function (req, res) {
-    var newKlient = req.body;
-    klients.push(newKlient);
+var Klient = mongoose.model("Klient", new Schema({
+    description: String,
+    tags: [ String ]
+}));
+
+app.get("/klients", async (req, res) => {
+    await Client.find()
+                .then(async (Klients) => {
+					res.json(Klients);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+});
+
+app.post("/klients", async (req, res) => {
+	console.log(req.body);
+	let newKlient = new Klient({
+        "description": req.body.description, 
+        "tags": req.body.tags
+    });
+	
+	await newKlient.save()
+                   .then(async (result) => {
+                       await Klient.find()
+                           .then(async (result) => {
+                               res.json(result);
+                           })
+                           .catch(async (err) => {
+                               res.send(err);
+                           });
+                   })
+                   .catch(async (err) => {
+                       console.log(err);
+                       res.send("ERROR");
+                   });
 });
